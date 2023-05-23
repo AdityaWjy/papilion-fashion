494be005b0948c024a5561422760242d2c8798de
ipologize <adamfadzal662@gmail.com>
2023-05-23 21:40:41 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ipologize.github.io
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi", "Cursive"]
+      },
+      colors: {
+        primary1: "#003E29",
+        primary2: "#E4BBA1",
+        primary3: "#D9D9D9",
+        borderColor: "#787878",
+        bgColor: "#EDEDED",
+        bgGreen: "#023423",
+
+
+
+      }
+    },
+  },
+  plugins: [],
+}
+
